dd8f2549780228d5a4617467373c1c48e08b2cf1
Tom Wojcik <tomasz.wooojcik@gmail.com>
2021-01-02 15:22:20 +0100

    make env var PUBLIC_URL optional

--- template/src/renderSitemapToString.js
+++ template/src/renderSitemapToString.js
@@ -3,29 +3,30 @@ import {SitemapStream, EnumChangefreq, streamToPromise} from "sitemap";
 
 async function renderSitemapToString({routes}) {
   let publicURL = process.env.PUBLIC_URL
+  if (publicURL) {
+    let {paths} = await crawl({
+      routes,
+      root: '/',
+    })
+    const sitemap = new SitemapStream({hostname: publicURL})
 
-  let {paths} = await crawl({
-    routes,
-    root: '/',
-  })
-  const sitemap = new SitemapStream({hostname: publicURL})
-
-  for (let pathname of paths.sort()) {
-    if ((pathname === `/about`) || (pathname.startsWith(`/posts`))) {
-      sitemap.write({
-        url: pathname,
-        changefreq: EnumChangefreq.DAILY,
-        priority: 0.3
-      })
+    for (let pathname of paths.sort()) {
+      if ((pathname === `/about`) || (pathname.startsWith(`/posts`))) {
+        sitemap.write({
+          url: pathname,
+          changefreq: EnumChangefreq.DAILY,
+          priority: 0.3
+        })
+      }
     }
+    sitemap.end()
+
+    // Readable.from is available on node ^12.3.0 and netlify deployments break with ^11.15.0
+    // https://github.com/nodejs/node/issues/28693
+    // https://github.com/frontarm/navi/issues/92
+
+    return await streamToPromise(sitemap).then(buffer => buffer.toString())
   }
-  sitemap.end()
-
-  // Readable.from is available on node ^12.3.0 and netlify deployments break with ^11.15.0
-  // https://github.com/nodejs/node/issues/28693
-  // https://github.com/frontarm/navi/issues/92
-
-  return await streamToPromise(sitemap).then(buffer => buffer.toString())
 }
 
 export default renderSitemapToString
